ff94b05d607b75c8a4750bca02809dd796c319d5
tbrundige <77417590+tbrundige@users.noreply.github.com>
2021-11-07 00:38:04 -0600

    added pipe option
    added -p option which removes the encoded and decode labels. Useful for piping to file etc...

--- bin/index.js
+++ bin/index.js
@@ -1,39 +1,72 @@
 #!/usr/bin/env node
 
-//Import yargs to pass arguments to nodeapp
+// Import yargs to pass arguments to nodeapp
 const yargs = require("yargs");
 
-//Define arguments
+// Define arguments
 const options = yargs
   .usage("Usage: -e <stringToEncode>")
   .option("e", {
-    alias: "",
-    describe: "Encode String",
-    type: "stringToEncode",
+    alias: "encode",
+    describe: "String to Encode",
+    type: "string",
     demandOption: false,
   })
   .usage("Usage: -d <base64ToDecode>")
-  .option("d", {
-    alias: "",
-    describe: "Decode String",
-    type: "stringToDecode",
+  .option("decode", {
+    alias: "decode",
+    describe: "String to Decode",
+    type: "string",
+    demandOption: false,
+  })
+  .usage("Usage: -p")
+  .option("p", {
+    alias: "pipe",
+    describe: "Pipe option, removes the encode and decode labels on output ",
+    type: "boolean",
     demandOption: false,
   }).argv;
 
-//Get the data from arguments.
-//todo - get data from alias to enable usage of both short form and long form eg. -e --encode, -d --decode
-let stringToEncode = options.e;
-let stringToDecode = options.d;
-
-
-//Encode ASCII string to base64
-if (options.e !== undefined) {
-  console.log("Base64 Encoded: " + Buffer.from(stringToEncode).toString('base64'));
+function base64Encoder(string) {
+  try {
+    let encodedString = Buffer.from(string).toString("base64");
+    return encodedString;
+  } catch (err) {
+    return;
+  }
 }
 
-//Decode base64 string to ASCII test
-if (options.d !== undefined) {
-  console.log("Decoded Text: " + Buffer.from(stringToDecode, "base64").toString());
-} 
+function base64Decoder(string) {
+  try {
+    let decodedString = Buffer.from(string, "base64").toString();
+    return decodedString;
+  } catch (err) {
+    return;
+  }
+}
+
+//todo - get data from alias to enable usage of both short form and long form eg. -e --encode, -d --decode
+
+// Perform the encoding and decoding.
+let encodedString = base64Encoder(options.e);
+let decodedString = base64Decoder(options.d);
+
+if (encodedString !== undefined) {
+  if (options.p) {
+    console.log(encodedString);
+  } else {
+    console.log("Base64 Encoded: " + encodedString);
+  }
+}
+
+if (decodedString !== undefined) {
+  if (options.p) {
+    console.log(decodedString);
+  } else {
+    console.log("Decoded String: " + decodedString);
+  }
+}
+
+//console.log(options)
 
 //todo - Provide feedback if invlaid parameters are used
