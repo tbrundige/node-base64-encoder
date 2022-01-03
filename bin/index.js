28be7d1adaf475b0c65c0ca90e28d7bdf9ec27ba
tbrundige <77417590+tbrundige@users.noreply.github.com>
2022-01-02 22:36:41 -0700

    rewrite to exclude third party libs

--- bin/index.js
+++ bin/index.js
@@ -1,72 +1,61 @@
 #!/usr/bin/env node
 
-// Import yargs to pass arguments to nodeapp
-const yargs = require("yargs");
+// Get the arguments.
+const appArgs = process.argv.slice(2);
 
-// Define arguments
-const options = yargs
-  .usage("Usage: -e <stringToEncode>")
-  .option("e", {
-    alias: "encode",
-    describe: "String to Encode",
-    type: "string",
-    demandOption: false,
-  })
-  .usage("Usage: -d <base64ToDecode>")
-  .option("decode", {
-    alias: "decode",
-    describe: "String to Decode",
-    type: "string",
-    demandOption: false,
-  })
-  .usage("Usage: -p")
-  .option("p", {
-    alias: "pipe",
-    describe: "Pipe option, removes the encode and decode labels on output ",
-    type: "boolean",
-    demandOption: false,
-  }).argv;
+// Create an object to store helptext for each flag. 
+const usageInfo = {
+  "e": "Use -e <stringToEncode> to base64 encode a ASCII string.",
+  "d": "Use -d <stringToDecode> to decode a base64 encoded string.",
+  "h": "Use -h to display usage info."
+};
 
-function base64Encoder(string) {
+// Create an object to store possible errors.
+const error = {
+  "default": "Error: Please use -h or --help to view usage info.",
+  "tooManyArguments": "Error: Too many arguments. Please use -h or --help for usage info.",
+  "tooFewArguments": "Error: Nothing to encode/decode or flag not specifed.",
+};
+
+// Check if sufficient info is provided e.g a flag and a string to encode or decode.
+// Return error message if too few or too many arugments.
+// Proceed to to encode or decode provided string.
+if (appArgs.length > 2) {
+  console.log(error.tooManyArguments);
+} else if (appArgs.length < 2 && appArgs[0] !== "-h") {
+  console.log(error.tooFewArguments);
+} else {
+  if (appArgs[0] === "-e") {
+    let encodedString = base64Encoder(appArgs[1]);
+    console.log(encodedString);
+  } else if (appArgs[0] === "-d") {
+    let decodedString = base64Decoder(appArgs[1]);
+    console.log(decodedString);
+  } else if (appArgs[0] === "-h") {
+    for (const helpText in usageInfo) {
+      console.log(usageInfo[helpText]);
+    }
+  } else {
+    console.log(error.default);
+  }
+}
+
+// This function encodes the provided string to Base64.
+function base64Encoder(stringToEncode) {
   try {
-    let encodedString = Buffer.from(string).toString("base64");
+    let encodedString = Buffer.from(stringToEncode).toString("base64");
     return encodedString;
   } catch (err) {
     return;
   }
 }
 
-function base64Decoder(string) {
+// This function decodes Base64 strings.
+function base64Decoder(stringToDecode) {
   try {
-    let decodedString = Buffer.from(string, "base64").toString();
+    let decodedString = Buffer.from(stringToDecode, "base64").toString();
     return decodedString;
   } catch (err) {
     return;
   }
 }
-
-//todo - get data from alias to enable usage of both short form and long form eg. -e --encode, -d --decode
-
-// Perform the encoding and decoding.
-let encodedString = base64Encoder(options.e);
-let decodedString = base64Decoder(options.d);
-
-if (encodedString !== undefined) {
-  if (options.p) {
-    console.log(encodedString);
-  } else {
-    console.log("Base64 Encoded: " + encodedString);
-  }
-}
-
-if (decodedString !== undefined) {
-  if (options.p) {
-    console.log(decodedString);
-  } else {
-    console.log("Decoded String: " + decodedString);
-  }
-}
-
-//console.log(options)
-
-//todo - Provide feedback if invlaid parameters are used
